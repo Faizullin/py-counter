0beb216aaf63dcae41abdd8adf523957df52f455
Faizullin <86420831+Faizullin@users.noreply.github.com>
2022-06-04 18:15:12 +0600

    Delete py-counter directory

--- py-counter/server/src/tele.py
+++
@@ -1,8 +0,0 @@
-import telebot
-class TeleBot(telebot.TeleBot):
-	def __init__(self,config):
-		telebot.TeleBot.__init__(self,config.TELEGRAM_TOKEN)
-		del config
-	def send(self,message,text):
-		#user_markup = telebot.types.ReplyKeyboardMarkup(True, False); user_markup.row('Помощь')
-		self.send_message(message.chat.id,text.lower().capitalize())
